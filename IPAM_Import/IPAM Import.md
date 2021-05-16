0d535761f9470185110369f8a481cd84be39e95c
Paul <email@paul-williams.me.uk>
2021-05-16 23:10:48 +0100

    Added markdown file and added comments to code.

---
+++ IPAM_Import/IPAM Import.md
@@ -0,0 +1,19 @@
+# IPAM Import
+
+### Description
+
+Program to read Excel data in an array and upload somewhere using API
+Need to provide path to .xlsx file
+Usage: python3 ReadIPAM.py ~/Documents/IPAM.xls
+
+### ToDo:
+
+- [x] Read sheet from argument
+- [x] Read sheet names and display menu
+- [x] Create inputs for cell references
+- [x] Read sheet into array
+- [x] Print preview of contents
+- [ ] Validate cell reference inputs
+- [ ] Validate preview range
+- [ ] Prevent empty lines being added to array
+- [ ] Integrate API Upload
--- IPAM_Import/ReadIPAM.py
+++ IPAM_Import/ReadIPAM.py
@@ -1,3 +1,7 @@
+###### Program to read Excel data in an array and upload somewhere using API
+###### Need to provide path to .xlsx file
+###### Usage: python3 ReadIPAM.py ~/Documents/IPAM.xlsx
+
 import sys
 import pprint
 import os.path
@@ -20,6 +24,7 @@ EndRow = int()
 NumRows = int()
 EndRowNum = int()
 
+# Create array for interating column references
 letters = [
            "a","b","c","d","e","f","g","h","i","k",
            "l","m","n","o","p","q","r","s","t","u",
@@ -37,11 +42,13 @@ else:
     exit()
 
 
-#Assign References to Sheet Names in 2d array
+# Assign References to Sheet Names in 2D array - for the menu
+# Loop Counter used to provide an ID number for each sheet name
 for ExcelSheetName in myworkbook.sheetnames:
     LoopCounter += 1
     MySheets.append([LoopCounter,ExcelSheetName])
 
+#Display a menu to help select a sheet name - adding a space for numbers <10 to align sheet names
 print("Sheets Names:")
 for x in range(len(MySheets)):
     if x < 9:
@@ -49,7 +56,9 @@ for x in range(len(MySheets)):
     else:
         Padding_Char = ""
     print(str(MySheets[x][0]) + Padding_Char + " -- " + str(MySheets[x][1]))
- 
+
+
+# Ask user for selected sheet ID, validation to check entry is a number and within range
 while True:      # keep looping until we break out of the loop
     try:
         SelectedSheet = int(input("\nSelect Sheet ID: "))
@@ -60,39 +69,39 @@ while True:      # keep looping until we break out of the loop
     except ValueError:
         print("Please enter an integer!")
 
+# Set the workingsheet to the selected sheet
 SelectSheetName = MySheets[SelectedSheet-1][1]
-
-
-print("Selected Sheet Name: " + SelectSheetName)
 myworksheet = myworkbook[SelectSheetName]
 
+# Dispaly the selected sheet
+print("Selected Sheet Name: " + SelectSheetName)
 
-###
+### Obtain left column, starting row and the number of rows in order to calculate loop range
 KeyColumn = input("Key Column (Column with IP Addresses):")
 StartRow = input("Starting Row:")
 NumRows = input("Number of Rows: ")
 EndRowNum = int(StartRow) + int(NumRows)
 ColIndexNum = letters.index(KeyColumn.lower())
-DescColumn = letters[ColIndexNum + 1]
+DescColumn = letters[ColIndexNum + 1] # Increments row column (e.g. A + 1 = B)
 
-print("Desc Col: " + str(DescColumn.upper())) 
-
-
+# Loop To read values into a  2D array called MyIPs
 for loop in range(int(StartRow), EndRowNum):
-    my_ip_cell = KeyColumn + str(loop)
-    my_desc_cell = DescColumn.upper() + str(loop)
+    my_ip_cell = KeyColumn + str(loop) #Sets the first cell to be read 
+    my_desc_cell = DescColumn.upper() + str(loop) #Sets the second cell to be read
     MyIPs.append([myworksheet[my_ip_cell].value, myworksheet[my_desc_cell].value])
 
+# Reset LoopCounter used previous in sheet indexing
 LoopCounter = 0
 
+# Loop to display a subset of the output for validation purposes, before proceeding with an upload
+# First X and last X are printed to ensure the range looks correct
+# LoopCouter is used to control how many lines are printed
 for x in range(len(MyIPs)):
     LoopCounter += 1
-    if LoopCounter <= PreviewAmount:
+    if LoopCounter <= PreviewAmount: # Print if outputted lines are less than the set preview amount
         print(str(MyIPs[x][0]) + "    ---- " + str(MyIPs[x][1]))
-        if LoopCounter == PreviewAmount: print("\n ------- Ommited ------- \n")
-    elif x > (len(MyIPs)-PreviewAmount):
-        print(str(MyIPs[x][0]) + "    ---- " + str(MyIPs[x][1]))
+        if LoopCounter == PreviewAmount: print("\n ------- Ommited ------- \n") # Print dividing line
+    elif x > (len(MyIPs)-PreviewAmount): # Print last X amount of entries
+        print(str(MyIPs[x][0]) + "    ---- " + str(MyIPs[x][1])) 
 ####
 
-print("End Row: " + str(EndRowNum))
-pp.pprint(MyIPs)
